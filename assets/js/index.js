4b4959aa37b342b54ef5eee4e3176c5e5e8e15ed
cwen13 <cwenrich13@gmail.com>
2022-12-18 21:54:20 -0500

    Get weather api working and results page started

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,10 +4,12 @@
 let searchBtn = $("#search");
 let cityEl = $("#city");
 let stateEl = $("#state");
+let latlong;
+let cityLatLong = [];
 
 let apiBase = "https://api.openweathermap.org/";
 let apiKey = "83a44da7964246bbf900a3b2168f29ce";
-let apiBaseWeather = apiBase + "data/2.5/forcast?";
+let apiBaseWeather = apiBase + "data/2.5/forecast?";
 let apiBaseLatLon = apiBase + "geo/1.0/direct?";
 
 //get state and city informaiton
@@ -18,47 +20,58 @@ let apiBaseLatLon = apiBase + "geo/1.0/direct?";
 
 // from geocogin
  
-function getData(url) {
-  return fetch(url)
-    .then(function(response) {
-      return response.json();
-    })
-    .then(function (data) {
-      return data;
-    });
-}
-
+// function getData(url) {
+//   fetch(url)
+//     .then(function(response) {
+//       return response.json();
+//     })
+//     .then(function (data) {
+//       geocode = data;
+//     }).then(function(gJson)
+// 	    latitude, longitude = getLatLon(gJson, city))}
+// }
+// 
 function getLatLon(geoJson, state) {
   for (let i=0; i<geoJson.length; i++) {
-    console.log(geoJson[i]["state"]);
     if (geoJson[i]["state"] == state) {
-      console.log(geoJson[i]["state"]);
-      return [geoJson[i]["lat"],geoJson[i]["lon"]];
+      return [geoJson[i]["lat"].toFixed(2)
+	      ,geoJson[i]["lon"].toFixed(2)];
     }
   }
-  return "State not found";
+  return console.log("State not found");
 }
     
 
+
 function handlerGetData(event){
   event.preventDefault();
+  if (localStorage.getItem("cityLatLong")) {
+    cityLatLong = JSON.parse(localStorage.getItem("cityLatLong"));
+  }
+  
   let city = cityEl.val();
   let state = stateEl.val();
   console.log(city +", "+state);
   let apiLatLon = `${apiBaseLatLon}q=${city},${state}&limit=10&appid=${apiKey}`;
   console.log(apiLatLon);
-  let geocode = getData(apiLatLon);
-  
-  var latitude, longitude = getLatLon(geocode,state);
-  let apiWeather = `${apiBaseWeather}lat=${latitude}&lon=${longitude}&cnt=6&units=imperial&appid=${apiKey}`;
-  console.log(apiWeather);
-  let weather = getData(apiWeather);
-  
-  date = Date.parse(document.querySelector("#startDate").value);
-  localStorage.setItem("weather", JSON.stringify(weather));
+   fetch(apiLatLon)
+    .then(response => response.json())
+    .then((data) =>{
+      latlong = getLatLon(data,state);
+      console.log("LAt long : " + latlong);
+      cityLatLong.push([city+","+state, latlong]);
+      localStorage.setItem("cityLatLong", JSON.stringify(cityLatLong));
+      let apiWeather = `${apiBaseWeather}lat=${latlong[0]}&lon=${latlong[1]}&units=imperial&appid=${apiKey}`;
+      console.log(apiWeather);
+      fetch(apiWeather)
+	.then(response => response.json())
+	.then((data) => {
+	  localStorage.setItem("weatherResponse", JSON.stringify(data["list"]));
+	});
+    })
+    .then(() => { window.location.href = "./results.html";});
   return 0;
 }
 
 
 searchBtn[0].addEventListener('click', handlerGetData);
-console.log("passed the function");
--- assets/js/results.js
+++ assets/js/results.js
@@ -1,15 +1,41 @@
 
-let forcastEl = $(".weekforcast");
 
-let weather = {date: "0923",
-	       icon: "$",
-	       temp: "34.4",
-	       wind: "23",
-	       humidity: "34%"
-	      }
+let weatherData = JSON.parse(localStorage.getItem("weatherResponse"));
+console.log(weatherData);
 
-function buildEntry (weather) {
-  let weekForcast = $(".week-forcast");
+function pullStats(weatherEntry) {
+  let entry = {date:"",
+	       icon:"",
+	       temp:"",
+	       wind:"",
+	       humidity:""
+	      };
+  let date = new Date(weatherEntry["dt"]*1000);
+  entry["date"] = date.getFullYear()
+    +'/'+('0'+(date.getMonth()+1)).slice(-2)
+    +'/'+('0'+date.getDate()).slice(-2);
+  entry["icon"] = weatherEntry["weather"][0]["icon"];
+  entry["temp"] = weatherEntry["main"]["temp"];
+  // add in line to get direction in NWSE
+  entry["wind"] = weatherEntry["wind"]["speed"];
+  entry["humidity"] = weatherEntry["main"]["humidity"];
+
+  return entry;
+}
+function buildToday (weatherNow) {
+  // reach inot global to get weatherToday
+  let date = $("#today .date").text("Date: "+weatherNow["date"]);
+  let icon = $("#today .icon").text(weatherNow["icon"]);
+  let temp = $("#today .temp").text("Temperature: "+weatherNow["temp"]);
+  let wind = $("#today .wind").text("wind speed: "+weatherNow["wind"]);
+  let humidity = $("#today .humidity").text("Humidity: "+weatherNow["humidity"]);
+  return 0;
+}
+
+
+function buildForecast (weather) {
+  // reach into gloaal for weeather variable
+  let weekForcast = $(".week-forecast");
   let section = $("<section>");
   let date = $("<section>").text("DATE:");
   let icon = $("<section>");
@@ -31,7 +57,23 @@ function buildEntry (weather) {
   section.append(humidity);
   console.log(section);
   weekForcast.append(section);
-  
+  return 0;
 }
 
-buildEntry(weather);
+
+function main () {
+  
+ // buildToday(pullStats());
+  for (let i=0;i<weatherData.length; i++) {
+    console.log(weatherData[i]);
+    let readingTime = (weatherData[i]["dt_txt"]).split(" ")[1];
+    if (readingTime === "12:00:00"){
+      buildForecast(pullStats(weatherData[i]));
+    }
+  }
+}
+
+
+main();
+
+
