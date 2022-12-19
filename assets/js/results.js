64ed2a45ec78107a284600a549333a48c2df6558
cwen13 <cwenrich13@gmail.com>
2022-12-19 01:02:24 -0500

    update results page with list of previous cities

--- assets/js/results.js
+++ assets/js/results.js
@@ -2,6 +2,7 @@
 
 let weatherData = JSON.parse(localStorage.getItem("weatherResponse"));
 let weatherToday = JSON.parse(localStorage.getItem("weatherToday"));
+let prevCities = JSON.parse( localStorage.getItem("cityLatLong"));
 
 function pullStats(weatherEntry) {
   let entry = {date:"",
@@ -22,10 +23,22 @@ function pullStats(weatherEntry) {
 
   return entry;
 }
+    
 function buildToday (weatherNow) {
+  function getIcon(iconTag) {
+    let iconURL = "http://openweathermap.org/img/wn/"+iconTag+".png";
+    let icon = $("<img>");
+    icon.attr("src",iconURL);
+    $("#today .date").append(icon);
+    return 0;
+  }
+
   // reach inot global to get weatherToday
-  
-  let date = $("#today .date").text("Now: "+weatherNow["weather"][0]["icon"]);
+  let city = $("<h2>").text(weatherNow["name"]);
+  city.insertBefore($("#today"))
+  let date = $("#today .date").text("Now: ");
+  getIcon(weatherNow["weather"][0]["icon"]);
+    
   let temp = $("#today .temp").text("Temperature: "+weatherNow["main"]["temp"]);
   let wind = $("#today .wind").text("wind speed: "+weatherNow["wind"]["speed"]);
   let humidity = $("#today .humidity").text("Humidity: "+weatherNow["main"]["humidity"]);
@@ -34,17 +47,18 @@ function buildToday (weatherNow) {
 
 
 function buildForecast (weather) {
+  
   // reach into gloaal for weeather variable
   let weekForcast = $(".week-forecast");
   let section = $("<section>");
   let date = $("<section>").text("DATE:");
-  let icon = $("<section>");
+  let icon = $("<img>");
   let temp = $("<section>");
   let wind = $("<section>");
   let humidity = $("<section>");
 
   date.text("Date: " + weather["date"]);
-  icon.text("icon " +weather["icon"]);
+  icon.attr("src","http://openweathermap.org/img/wn/"+weather["icon"]+".png")
   temp.text("Temperature: " +weather["temp"]);
   wind.text("Wind speed: " + weather["wind"]);
   humidity.text("Humidity: " +weather["humidity"]);
@@ -59,8 +73,25 @@ function buildForecast (weather) {
   return 0;
 }
 
+function buildPrevCities(pCity) {
+  let city = pCity[0].split(",")[0];
+  let cityEntry = $("<li>");
+  let cityEl = $("<button>");
+  cityEl.text(city);
+  cityEl.attr("class", "button is-info coulmns");
+  
+  $(".prev-cities").append(cityEntry.append(cityEl));
+  return 0;
+}
 
 function main () {
+
+  if (prevCities) {
+    for (let i=0; i<prevCities.length; i++){
+      console.log(prevCities);
+      buildPrevCities(prevCities[i]);
+    }
+  }
   
   buildToday(weatherToday);
   for (let i=0;i<weatherData.length; i++) {
